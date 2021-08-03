12ba8227711d0f1d7fdaa6036cc38ad6838f1dff
Hundoshi <nestour95@gmail.com>
2021-08-04 01:01:29 +0900

    updating test

--- 01_variable/src/kr/co/ezen/run/Run.java
+++ 01_variable/src/kr/co/ezen/run/Run.java
@@ -20,7 +20,7 @@ public class Run {
 		C_keyboardInput ck = new C_keyboardInput();
 		//ck.inputScanner1();
 		//ck.inputScanner2(); // 여러대의 스캐너를 작동하는 것은 좋지않다. 어차피 스캐너 하나로 다 할 수 있음. close이후에는 스캐너 불러올수없음.
-		//ck.inputScanner3(); //인지용
+		//ck.inputScanner3(); //인지용인지용
 		D_Cast bd = new D_Cast();
 		bd.rule1();
 		bd.rule2();
